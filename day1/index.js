c8375ff5b52c122c9a8693666c3819d4aa9c902c
sergioedo <sergio.edo@gmail.com>
2021-12-01 19:42:46 +0100

    day 1 - basic completed

--- day1/index.js
+++ day1/index.js
@@ -1,2 +1,8 @@
-const getLargestMeasures = (measures) => {}
+const getLargestMeasures = (measures) => {
+    return measures.reduce((acc, curr, idx, arr) => {
+        if (idx === 0) return 0
+        if (curr > arr[idx - 1]) return acc + 1
+        return acc
+    }, 0)
+}
 module.exports = getLargestMeasures
