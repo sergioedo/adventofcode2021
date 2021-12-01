a01f5cde71a71eb7638d367f4d91cc70f424bf82
sergioedo <sergio.edo@gmail.com>
2021-12-01 20:27:02 +0100

    day 1 - part 2 basic test

--- day1/index.js
+++ day1/index.js
@@ -5,4 +5,16 @@ const getLargestMeasures = (measures) => {
         return acc
     }, 0)
 }
-module.exports = getLargestMeasures
+
+const get3MeasurementSlidingWindow = (measures) => {
+    let count = 0
+    measures.reduce((acc, curr, idx, arr) => {
+        if (idx + 2 >= measures.length) return 0 // no more 3-measurements
+        const currMeasure = arr[idx] + arr[idx + 1] + arr[idx + 2]
+        if (idx > 0 && currMeasure > acc) count++
+        return currMeasure
+    }, 0)
+    return count
+}
+
+module.exports = { getLargestMeasures, get3MeasurementSlidingWindow }
--- day1/index.test.js
+++ day1/index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const getLargestMeasures = require('./index')
+const { getLargestMeasures, get3MeasurementSlidingWindow } = require('./index')
 
 test('basic test getLargestMeasures', () => {
     const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
@@ -11,4 +11,10 @@ test('complete solution getLargestMeasures', () => {
     const input = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(Number)
     const expected = 1791
     expect(getLargestMeasures(input)).toBe(expected)
+})
+
+test('basic test three-measurement sliding window', () => {
+    const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    const expected = 5
+    expect(get3MeasurementSlidingWindow(input)).toBe(expected)
 })
