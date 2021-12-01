e6abc905e8a1ba126788574fa685fa8a9cf4dd36
sergioedo <sergio.edo@gmail.com>
2021-12-01 19:51:07 +0100

    day 1 - part 1 completed

--- day1/index.test.js
+++ day1/index.test.js
@@ -1,7 +1,14 @@
+const fs = require('fs')
 const getLargestMeasures = require('./index')
 
-test('basic test', () => {
+test('basic test getLargestMeasures', () => {
     const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     const expected = 7
+    expect(getLargestMeasures(input)).toBe(expected)
+})
+
+test('complete solution getLargestMeasures', () => {
+    const input = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(Number)
+    const expected = 1791
     expect(getLargestMeasures(input)).toBe(expected)
 })
