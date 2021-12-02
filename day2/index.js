5c1816235921290e66bb4c0548f3caa01cbd2cf1
sergioedo <sergio.edo@gmail.com>
2021-12-02 20:57:30 +0100

    day 2 - part2

--- day2/index.js
+++ day2/index.js
@@ -14,4 +14,22 @@ const pilotSubmarine = (movements) => {
     return finalLocation.hPosition * finalLocation.depth
 }
 
-module.exports = { pilotSubmarine }
+const pilotSubmarineWithAim = (movements) => {
+    const initialLocation = {
+        hPosition: 0,
+        depth: 0,
+        aim: 0
+    }
+    const finalLocation = movements.reduce((location, movement) => {
+        const { move, positions } = movement
+        return {
+            aim: location.aim + +(move === 'up' ? -positions : move === 'down' ? positions : 0),
+            hPosition: location.hPosition + (move === 'forward' ? positions : 0),
+            depth: location.depth + (move === 'forward' ? location.aim * positions : 0)
+        }
+    }, initialLocation)
+
+    return finalLocation.hPosition * finalLocation.depth
+}
+
+module.exports = { pilotSubmarine, pilotSubmarineWithAim }
--- day2/index.test.js
+++ day2/index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { pilotSubmarine } = require('./index')
+const { pilotSubmarine, pilotSubmarineWithAim } = require('./index')
 
 test('basic course', () => {
     const course = [
@@ -16,6 +16,25 @@ test('basic course', () => {
 
 test('complete solution pilotSubmarine', () => {
     const course = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(line => ({ move: line.split(' ')[0], positions: Number(line.split(' ')[1]) }))
-    const expected = 0
+    const expected = 1727835
     expect(pilotSubmarine(course)).toBe(expected)
+})
+
+test('basic course with aim', () => {
+    const course = [
+        { move: 'forward', positions: 5 },
+        { move: 'down', positions: 5 },
+        { move: 'forward', positions: 8 },
+        { move: 'up', positions: 3 },
+        { move: 'down', positions: 8 },
+        { move: 'forward', positions: 2 }
+    ]
+    const expected = 900
+    expect(pilotSubmarineWithAim(course)).toBe(expected)
+})
+
+test('complete solution pilotSubmarine with aim', () => {
+    const course = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(line => ({ move: line.split(' ')[0], positions: Number(line.split(' ')[1]) }))
+    const expected = 1544000595
+    expect(pilotSubmarineWithAim(course)).toBe(expected)
 })
