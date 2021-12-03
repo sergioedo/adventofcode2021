b9a3ec4c119ab805d0cf3dc9655a51270ab0fc55
sergioedo <sergio.edo@gmail.com>
2021-12-03 20:35:32 +0100

    day 3 - part 2

--- day3/index.js
+++ day3/index.js
@@ -1,13 +1,34 @@
-const getPowerConsumption = (report) => {
+const getBitsBalance = (report) => {
     const initialBitsBalance = report[0].split('').map(bit => 0) // starts with 0 balance in 0 vs 1
-    const bitsBalance = report.reduce((prevBalance, currentBits) => {
+    return report.reduce((prevBalance, currentBits) => {
         return prevBalance.map((bit, idx) => bit + (currentBits[idx] === '1' ? 1 : -1))
     }, initialBitsBalance)
+}
 
+const getPowerConsumption = (report) => {
+    const bitsBalance = getBitsBalance(report)
     const gammaRate = parseInt(bitsBalance.map(bitBalance => bitBalance >= 0 ? '1' : '0').join(''), 2)
     const epsilonRate = parseInt(bitsBalance.map(bitBalance => bitBalance >= 0 ? '0' : '1').join(''), 2)
-    console.log(gammaRate, epsilonRate)
     return gammaRate * epsilonRate
 }
 
-module.exports = { getPowerConsumption }
+const getRating = (report, getBitFilterValue) => {
+    const initialBits = report[0].split('')
+    const rating = initialBits.reduce((rateCandidates, bit, bitIndex) => {
+        if (rateCandidates.length === 1) return rateCandidates // As there is only one number left => oxygen generator rating found
+        const bitsBalance = getBitsBalance(rateCandidates)
+        const bitFilterValue = getBitFilterValue(bitsBalance[bitIndex])
+        return rateCandidates.filter(value => value[bitIndex] === bitFilterValue)
+    }, report)
+
+    return rating[0]
+}
+
+const getLifeSupportRating = (report) => {
+    const oxigenGeneratorRating = parseInt(getRating(report, bitBalance => bitBalance >= 0 ? '1' : '0'), 2)
+    const co2ScrubberRating = parseInt(getRating(report, bitBalance => bitBalance >= 0 ? '0' : '1'), 2)
+
+    return oxigenGeneratorRating * co2ScrubberRating
+}
+
+module.exports = { getPowerConsumption, getLifeSupportRating }
--- day3/index.test.js
+++ day3/index.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
-const { getPowerConsumption } = require('./index')
+const { getPowerConsumption, getLifeSupportRating } = require('./index')
 
-test('power consumption', () => {
+test('basic - power consumption', () => {
     const report = [
         '00100',
         '11110',
@@ -20,8 +20,33 @@ test('power consumption', () => {
     expect(getPowerConsumption(report)).toBe(expected)
 })
 
-test('complete solution power consumption', () => {
+test('complete solution - power consumption', () => {
     const report = fs.readFileSync('input.txt', 'UTF-8').split('\n')
     const expected = 4138664
     expect(getPowerConsumption(report)).toBe(expected)
+})
+
+test('basic - life support rating', () => {
+    const report = [
+        '00100',
+        '11110',
+        '10110',
+        '10111',
+        '10101',
+        '01111',
+        '00111',
+        '11100',
+        '10000',
+        '11001',
+        '00010',
+        '01010'
+    ]
+    const expected = 230
+    expect(getLifeSupportRating(report)).toBe(expected)
+})
+
+test('complete solution - life support rating', () => {
+    const report = fs.readFileSync('input.txt', 'UTF-8').split('\n')
+    const expected = 4273224
+    expect(getLifeSupportRating(report)).toBe(expected)
 })
