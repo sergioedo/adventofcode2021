edeae486c9f0a6d58c713b5cbefc27bf6af2a2e7
sergioedo <sergio.edo@gmail.com>
2021-12-05 14:21:41 +0100

    day 5 - part 2

--- day5/index.js
+++ day5/index.js
@@ -1,13 +1,13 @@
-const getLinePoints = (start, end) => {
+const getLinePoints = (start, end, includeDiagonals) => {
     const [startX, startY] = start.split(',')
     const [endX, endY] = end.split(',')
-    if (startX !== endX && startY !== endY) return [] // only horizontal and vertical lines
+    if (!includeDiagonals && startX !== endX && startY !== endY) return [] // only horizontal and vertical lines
     const incX = Number(endX) - Number(startX)
     const incY = Number(endY) - Number(startY)
     const steps = Math.max(Math.abs(incX), Math.abs(incY))
     const stepX = incX !== 0 ? incX > 0 ? 1 : -1 : 0
     const stepY = incY !== 0 ? incY > 0 ? 1 : -1 : 0
-    // console.log(startX, startY, endX, endY, incX, incY, steps)
+
     const points = []
     for (let i = 0; i <= steps; i++) {
         const coordX = Number(startX) + (i * stepX)
@@ -15,18 +15,17 @@ const getLinePoints = (start, end) => {
         // console.log(coordX, coordY)
         points.push(`${coordX},${coordY}`)
     }
-    // console.log(points)
+
     return points
 }
 
-const findHydroThermalVents = (locations, count) => {
+const findHydroThermalVents = (locations, count, includeDiagonals = false) => {
     const mapLocations = new Map()
     locations.map(line => {
         const [start, end] = line.split('->').map(value => value.trim())
-        getLinePoints(start, end).map(point => mapLocations.set(point, (mapLocations.get(point) || 0) + 1))
+        getLinePoints(start, end, includeDiagonals).map(point => mapLocations.set(point, (mapLocations.get(point) || 0) + 1))
         return [start, end]
     })
-    // console.log(Array.from(mapLocations.values()))
     return Array.from(mapLocations.values()).filter(value => value >= count).length
 }
 
--- day5/index.test.js
+++ day5/index.test.js
@@ -12,3 +12,15 @@ test('complete solution - hydrothermal vents', () => {
     const expected = 5442
     expect(findHydroThermalVents(locations, 2)).toBe(expected)
 })
+
+test('basic - hydrothermal vents with diagonals', () => {
+    const locations = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n')
+    const expected = 12
+    expect(findHydroThermalVents(locations, 2, true)).toBe(expected)
+})
+
+test('complete solution - hydrothermal vents with diagonals', () => {
+    const locations = fs.readFileSync('input.txt', 'UTF-8').split('\n')
+    const expected = 19571
+    expect(findHydroThermalVents(locations, 2, true)).toBe(expected)
+})
