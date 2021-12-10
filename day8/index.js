51253c4d45a7044d8dc2e82a3df15f20ff9d6d94
sergioedo <sergio.edo@gmail.com>
2021-12-10 01:57:56 +0100

    day 8 - part 2

--- day8/index.js
+++ day8/index.js
@@ -3,4 +3,71 @@ const countUniqueSegmentsNumbers = (outputValues) => {
     return outputValues.filter(value => uniqueSegmentsLengths.includes(value.length)).length
 }
 
-module.exports = { countUniqueSegmentsNumbers }
+const digits2Letters = {
+    abcefg: 0,
+    cf: 1,
+    acdeg: 2,
+    acdfg: 3,
+    bcdf: 4,
+    abdfg: 5,
+    abdefg: 6,
+    acf: 7,
+    abcdefg: 8,
+    abcdfg: 9
+}
+
+const sumOutputValues = (entries) => {
+    return entries.map(entry => {
+        const sortedPatterns = entry.patterns.map(pattern => pattern.split('').sort().join('')).sort((a, b) => a.length - b.length)
+        const pattern1 = sortedPatterns[0].split('')
+        const pattern7 = sortedPatterns[1].split('')
+        const pattern4 = sortedPatterns[2].split('')
+        const pattern8 = sortedPatterns[9].split('')
+
+        // diff 7 vs 1 --> "a"
+        const a = pattern7.filter(c => !pattern1.includes(c))[0]
+
+        // diff 0,6,9 vs 1 --> "c" --> "f"
+        const pattern6 = sortedPatterns
+            .filter(pattern => pattern.length === 6) // patterns 0, 6, 9
+            .map(pattern => pattern.split(''))
+            .filter(pattern => !pattern.includes(pattern1[0]) || !pattern.includes(pattern1[1]))[0] // 6 not includes "c"
+
+        const c = pattern1.filter(c => !pattern6.includes(c))[0]
+        const f = pattern1.filter(c => pattern6.includes(c))[0]
+
+        // 0,9 vs 4 --> 0 vs 4 --> "d"
+        const pattern10 = sortedPatterns
+            .filter(pattern => pattern.length === 6 && pattern !== pattern6.join('')) // patterns 0, 9
+            .map(pattern => pattern.split(''))
+            .filter(pattern => pattern4.filter(char => pattern.includes(char)).length !== 4)[0] // 9 includes 4, 0 dont
+        const d = pattern4.filter(char => !pattern10.includes(char))[0]
+
+        // 8 vs 9 --> e
+        const pattern9 = sortedPatterns
+            .filter(pattern => pattern.length === 6 && pattern !== pattern6.join('') && pattern !== pattern10.join(''))[0] // pattern 9
+            .split('')
+
+        const e = pattern8.filter(char => !pattern9.includes(char))[0]
+        const g = pattern9.filter(char => !pattern4.includes(char) && ![a, c, d, e, f].includes(char))[0]
+        const b = pattern8.filter(char => ![a, c, d, e, f, g].includes(char))
+
+        const conversion = {
+            [a]: 'a',
+            [b]: 'b',
+            [c]: 'c',
+            [d]: 'd',
+            [e]: 'e',
+            [f]: 'f',
+            [g]: 'g'
+        }
+
+        return Number(entry.outputValues.map(outputValue => outputValue
+            .split('').map(char => conversion[char]).sort().join(''))
+            .map(letters => digits2Letters[letters])
+            .join('')
+        )
+    }).reduce((acc, curr) => acc + curr, 0)
+}
+
+module.exports = { countUniqueSegmentsNumbers, sumOutputValues }
--- day8/index.test.js
+++ day8/index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { countUniqueSegmentsNumbers } = require('./index')
+const { countUniqueSegmentsNumbers, sumOutputValues } = require('./index')
 
 test('basic - count unique segments numbers', () => {
     const outputValues = fs.readFileSync('input-basic.txt', 'UTF-8')
@@ -19,4 +19,37 @@ test('complete solution - cheapest crabs alignment fuel incremental costs', () =
         .flat()
     const expected = 390
     expect(countUniqueSegmentsNumbers(outputValues)).toBe(expected)
+})
+
+test('basic - sum all correct output values - single line', () => {
+    const entries = ['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf']
+    // const entries = ['fdgcea dgefa efabdg fdgcba dab baefc begd ebdfa db bafgdce | gfbcda cgfadb badcefg eacfgd']
+        .map(line => ({
+            patterns: line.split('|')[0].trim().split(' ').map(values => values.split(' ')).flat(),
+            outputValues: line.split('|')[1].trim().split(' ').map(values => values.split(' ')).flat()
+        }))
+    const expected = 5353
+    expect(sumOutputValues(entries)).toBe(expected)
+})
+
+test('basic - sum all correct output values', () => {
+    const entries = fs.readFileSync('input-basic.txt', 'UTF-8')
+        .split('\n')
+        .map(line => ({
+            patterns: line.split('|')[0].trim().split(' ').map(values => values.split(' ')).flat(),
+            outputValues: line.split('|')[1].trim().split(' ').map(values => values.split(' ')).flat()
+        }))
+    const expected = 61229
+    expect(sumOutputValues(entries)).toBe(expected)
+})
+
+test('complete solution - sum all correct output values', () => {
+    const entries = fs.readFileSync('input.txt', 'UTF-8')
+        .split('\n')
+        .map(line => ({
+            patterns: line.split('|')[0].trim().split(' ').map(values => values.split(' ')).flat(),
+            outputValues: line.split('|')[1].trim().split(' ').map(values => values.split(' ')).flat()
+        }))
+    const expected = 1011785
+    expect(sumOutputValues(entries)).toBe(expected)
 })
