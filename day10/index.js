d09e06d7fcea53e94a44cfe88e91afa0bbb31937
sergioedo <sergio.edo@gmail.com>
2021-12-11 00:27:15 +0100

    day10 - part 2 (refactor)

--- day10/index.js
+++ day10/index.js
@@ -4,25 +4,28 @@ const charsScore = {
     ')': 3, ']': 57, '}': 1197, '>': 25137
 }
 
-const syntaxErrorScore = (chunks) => {
-    return chunks.map(line => {
-        const chars = line.split('')
-        const stack = []
-        for (let i = 0; i < chars.length; i++) {
-            const char = chars[i]
-            if (startChars.includes(char)) {
-                stack.push(char)
-            } else {
-                const startChar = stack.splice(-1)[0]
-                const idxChar = startChars.indexOf(startChar)
-                if (char !== endChars[idxChar]) {
-                    // return `Expected ${endChars[idxChar]} but found ${char}`
-                    return char
-                }
+const parseLine = (line) => {
+    const chars = line.split('')
+    const stack = []
+    for (let i = 0; i < chars.length; i++) {
+        const char = chars[i]
+        if (startChars.includes(char)) {
+            stack.push(char)
+        } else {
+            const startChar = stack.splice(-1)[0]
+            const idxChar = startChars.indexOf(startChar)
+            if (char !== endChars[idxChar]) {
+                return { stack, char, text: `Expected ${endChars[idxChar]} but found ${char}` }
             }
         }
-        // return `ommited line: ${line}`
-        return ''
+    }
+    return { stack, char: '', text: `ommited line: ${line}` }
+}
+
+const syntaxErrorScore = (chunks) => {
+    return chunks.map(line => {
+        const { char } = parseLine(line)
+        return char
     })
         .filter(char => char !== '')
         .reduce((acc, char) => acc + charsScore[char], 0)
@@ -34,20 +37,8 @@ const charsPoints = {
 
 const middleScore = (chunks) => {
     return chunks.map(line => {
-        const chars = line.split('')
-        const stack = []
-        for (let i = 0; i < chars.length; i++) {
-            const char = chars[i]
-            if (startChars.includes(char)) {
-                stack.push(char)
-            } else {
-                const startChar = stack.splice(-1)[0]
-                const idxChar = startChars.indexOf(startChar)
-                if (char !== endChars[idxChar]) {
-                    return []
-                }
-            }
-        }
+        const { stack, char } = parseLine(line)
+        if (char !== '') return []
         return stack.map(char => endChars[startChars.indexOf(char)]).reverse()
     })
         .filter(chars => chars.length !== 0)
