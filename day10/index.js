c8b6bcdfe2da23b23ad6a72fb9401b4479cf7e91
sergioedo <sergio.edo@gmail.com>
2021-12-11 00:26:36 +0100

    day 10 - part 2

--- day10/index.js
+++ day10/index.js
@@ -28,4 +28,32 @@ const syntaxErrorScore = (chunks) => {
         .reduce((acc, char) => acc + charsScore[char], 0)
 }
 
-module.exports = { syntaxErrorScore }
+const charsPoints = {
+    ')': 1, ']': 2, '}': 3, '>': 4
+}
+
+const middleScore = (chunks) => {
+    return chunks.map(line => {
+        const chars = line.split('')
+        const stack = []
+        for (let i = 0; i < chars.length; i++) {
+            const char = chars[i]
+            if (startChars.includes(char)) {
+                stack.push(char)
+            } else {
+                const startChar = stack.splice(-1)[0]
+                const idxChar = startChars.indexOf(startChar)
+                if (char !== endChars[idxChar]) {
+                    return []
+                }
+            }
+        }
+        return stack.map(char => endChars[startChars.indexOf(char)]).reverse()
+    })
+        .filter(chars => chars.length !== 0)
+        .map(chars => chars.reduce((acc, char) => (acc * 5) + charsPoints[char], 0))
+        .sort((a, b) => a - b)
+        .reduce((acc, value, idx, array) => array[Math.floor(array.length / 2)], 0)
+}
+
+module.exports = { syntaxErrorScore, middleScore }
--- day10/index.test.js
+++ day10/index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { syntaxErrorScore } = require('./index')
+const { syntaxErrorScore, middleScore } = require('./index')
 
 test('basic - syntax error score', () => {
     const chunks = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n')
@@ -11,4 +11,16 @@ test('complete solution - syntax error score', () => {
     const chunks = fs.readFileSync('input.txt', 'UTF-8').split('\n')
     const expected = 311949
     expect(syntaxErrorScore(chunks)).toBe(expected)
+})
+
+test('basic - middle score', () => {
+    const chunks = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n')
+    const expected = 288957
+    expect(middleScore(chunks)).toBe(expected)
+})
+
+test('complete solution - middle score', () => {
+    const chunks = fs.readFileSync('input.txt', 'UTF-8').split('\n')
+    const expected = 3042730309
+    expect(middleScore(chunks)).toBe(expected)
 })
