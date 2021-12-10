355d04e84874889d8df9e9918d2829dbfc5ae09d
sergioedo <sergio.edo@gmail.com>
2021-12-10 21:52:50 +0100

    day 9 - part 2

--- day9/index.js
+++ day9/index.js
@@ -11,4 +11,42 @@ const sumRiskLevels = (area) => {
         .flat()
         .reduce((sum, lowPoint) => sum + (lowPoint + 1), 0)
 }
-module.exports = { sumRiskLevels }
+
+const findNextBasinPoint = (area) => {
+    for (let row = 0; row < area.length; row++) {
+        for (let col = 0; col < area[row].length; col++) {
+            if (area[row][col] !== 9) return { row, col }
+        }
+    }
+    return { row: null, col: null } // no more points
+}
+
+const getBasin = (area, row, col) => {
+    if (area[row][col] === 9) return { updatedArea: area, basin: [] } // no basin here
+
+    const value = area[row][col]
+    area[row][col] = 9 // mark as counted as basin
+    const { updatedArea: leftUpdatedArea, basin: leftBasin } = col - 1 >= 0 ? getBasin(area, row, col - 1) : { updatedArea: area, basin: [] }
+    const { updatedArea: rightUpdatedArea, basin: rightBasin } = col + 1 < area[row].length ? getBasin(area, row, col + 1) : { updatedArea: leftUpdatedArea, basin: [] }
+    const { updatedArea: topUpdatedArea, basin: topBasin } = row - 1 >= 0 ? getBasin(area, row - 1, col) : { updatedArea: rightUpdatedArea, basin: [] }
+    const { updatedArea: downUpdatedArea, basin: downBasin } = row + 1 < area.length ? getBasin(area, row + 1, col) : { updatedArea: topUpdatedArea, basin: [] }
+
+    return { updatedArea: downUpdatedArea, basin: [value, ...leftBasin, ...rightBasin, ...topBasin, ...downBasin] }
+}
+
+const getBasins = (area, basins = []) => {
+    const { row, col } = findNextBasinPoint(area)
+    if (row === null || col === null) return { area, basins } // no more basins
+
+    const { updatedArea, basin } = getBasin(area, row, col)
+    return getBasins(updatedArea, [basin, ...basins])
+}
+
+const threeLargestBasins = (area) => {
+    return getBasins(area)
+        .basins.sort((a, b) => b.length - a.length)
+        .slice(0, 3)
+        .reduce((acc, basin) => acc * basin.length, 1)
+}
+
+module.exports = { sumRiskLevels, threeLargestBasins }
--- day9/index.test.js
+++ day9/index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { sumRiskLevels } = require('./index')
+const { sumRiskLevels, threeLargestBasins } = require('./index')
 
 test('basic - sum risk level of low points', () => {
     const area = fs.readFileSync('input-basic.txt', 'UTF-8')
@@ -13,6 +13,22 @@ test('complete solution - sum risk level of low points', () => {
     const area = fs.readFileSync('input.txt', 'UTF-8')
         .split('\n')
         .map(line => line.split('').map(Number))
-    const expected = 0
+    const expected = 506
     expect(sumRiskLevels(area)).toBe(expected)
+})
+
+test('basic - tree largest basins', () => {
+    const area = fs.readFileSync('input-basic.txt', 'UTF-8')
+        .split('\n')
+        .map(line => line.split('').map(Number))
+    const expected = 1134
+    expect(threeLargestBasins(area)).toBe(expected)
+})
+
+test('complete solution - tree largest basins', () => {
+    const area = fs.readFileSync('input.txt', 'UTF-8')
+        .split('\n')
+        .map(line => line.split('').map(Number))
+    const expected = 931200
+    expect(threeLargestBasins(area)).toBe(expected)
 })
