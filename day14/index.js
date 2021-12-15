ca22db71bf37bd91d5ad945dadeccbe478f95423
sergioedo <sergio.edo@gmail.com>
2021-12-15 22:37:45 +0100

    day 14 - part 2 (por fin!)

--- day14/index.js
+++ day14/index.js
@@ -1,30 +1,56 @@
-const buildPolymer = (input, pairs, steps) => {
-    if (steps === 0) return input
+const resultsCache = {}
 
-    const insertChars = input.split('').map((char, idx) => {
-        const pair = input.slice(idx, idx + 2)
-        return pairs[pair] || ''
+const mergeCounters = (counter1, counter2) => {
+    const counter = { ...counter1 }
+    Object.keys(counter2).forEach(char => {
+        counter[char] = (counter[char] || 0) + counter2[char]
     })
+    return counter
+}
 
-    const newInput = input.split('').map((char, idx) => char + insertChars[idx]).join('')
-    return buildPolymer(newInput, pairs, steps - 1)
+const countPairElements = (inputPair, rules, steps) => {
+    if (resultsCache[inputPair] && resultsCache[inputPair][steps]) return resultsCache[inputPair][steps]
+    if (steps === 0) return 0
+
+    const newChar = rules[inputPair]
+    const counter1 = countPairElements(inputPair.split('')[0] + newChar, rules, steps - 1)
+    const counter2 = countPairElements(newChar + inputPair.split('')[1], rules, steps - 1)
+    const counter = mergeCounters(counter1, counter2)
+    counter[newChar] = (counter[newChar] || 0) + 1
+
+    if (!resultsCache[inputPair]) resultsCache[inputPair] = {}
+    resultsCache[inputPair][steps] = counter
+    return counter
+}
+
+const clearCache = (cache) => {
+    Object.keys(cache).forEach(key => {
+        delete cache[key]
+    })
 }
 
 const diffPolymerElements = (template, rules, steps) => {
-    const pairs = rules.reduce((acc, rule) => {
+    const pairRules = rules.reduce((acc, rule) => {
         acc[rule[0]] = rule[1]
         return acc
     }, {})
 
-    const polymer = buildPolymer(template, pairs, steps)
-    const countElements = polymer.split('').reduce((acc, char) => {
-        acc[char] = (acc[char] | 0) + 1
-        return acc
-    }, {})
+    const inputPairs = template.split('').map((char, idx) => {
+        const pair = template.slice(idx, idx + 2)
+        return pair
+    }).filter(pair => pair.length >= 2)
+
+    clearCache(resultsCache)
+    const initialCounter = template.split('').reduce((counter, char) => ({ ...counter, [char]: (counter[char] || 0) + 1 }), {})
+    const countElements = inputPairs.reduce((counter, pair) => {
+        const pairCounter = countPairElements(pair, pairRules, steps)
+        return mergeCounters(counter, pairCounter)
+    }, initialCounter)
 
     const minCommon = Math.min(...Object.values(countElements))
     const maxCommon = Math.max(...Object.values(countElements))
 
     return maxCommon - minCommon
 }
+
 module.exports = { diffPolymerElements }
--- day14/index.test.js
+++ day14/index.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const { diffPolymerElements } = require('./index')
 
-test('basic - difference between most and least common polymer elements', () => {
+test('basic - difference between most and least common polymer elements after 10 steps', () => {
     const input = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n')
     const template = input[0]
     const rules = input.slice(2).map(line => line.split(' -> '))
@@ -9,10 +9,26 @@ test('basic - difference between most and least common polymer elements', () => 
     expect(diffPolymerElements(template, rules, 10)).toBe(expected)
 })
 
-test('complete solution - difference between most and least common polymer elements', () => {
+test('complete solution - difference between most and least common polymer elements after 10 steps', () => {
     const input = fs.readFileSync('input.txt', 'UTF-8').split('\n')
     const template = input[0]
     const rules = input.slice(2).map(line => line.split(' -> '))
     const expected = 3095
     expect(diffPolymerElements(template, rules, 10)).toBe(expected)
+})
+
+test('basic - difference between most and least common polymer elements after 40 steps', () => {
+    const input = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n')
+    const template = input[0]
+    const rules = input.slice(2).map(line => line.split(' -> '))
+    const expected = 2188189693529
+    expect(diffPolymerElements(template, rules, 40)).toBe(expected)
+})
+
+test('complete solution - difference between most and least common polymer elements after 40 steps', () => {
+    const input = fs.readFileSync('input.txt', 'UTF-8').split('\n')
+    const template = input[0]
+    const rules = input.slice(2).map(line => line.split(' -> '))
+    const expected = 3152788426516
+    expect(diffPolymerElements(template, rules, 40)).toBe(expected)
 })
