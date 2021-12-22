edde6d3aa7a77969478dc7140704b129501e4d07
sergioedo <sergio.edo@gmail.com>
2021-12-22 23:52:53 +0100

    day 21 - part 2

--- day21/index.js
+++ day21/index.js
@@ -1,23 +1,63 @@
-const endGame = (playersScore, turn) => {
-    return playersScore.some(score => score >= 1000)
+const endGameByScore = (winScore) => (playersScore) => {
+    return playersScore.some(score => score >= winScore)
 }
 
-const playGame = (playersStartPosition) => {
-    const numplayers = playersStartPosition.length
-    let turn = 1
-    let diceValue = 1
+const updatePlayerPositionScore = (playersStartPosition, playersStartScore, turn, diceValue) => {
+    const numPlayers = playersStartPosition.length
     const boardPositions = 10
-    const playersPosition = [...playersStartPosition]
-    const playersScore = Array(numplayers).fill(0)
 
-    while (!endGame(playersScore, turn)) {
-        const playerIndex = (turn - 1) % numplayers
-        playersPosition[playerIndex] = ((playersPosition[playerIndex] - 1 + ((diceValue * 3) + 3)) % boardPositions) + 1
-        playersScore[playerIndex] += playersPosition[playerIndex]
-        diceValue += 3
-        turn++
-    }
-    return { diceCount: diceValue - 1, playersScore }
+    const playersPosition = [...playersStartPosition]
+    const playersScore = [...playersStartScore]
+    const playerIndex = (turn - 1) % numPlayers
+    playersPosition[playerIndex] = ((playersPosition[playerIndex] - 1 + (diceValue)) % boardPositions) + 1
+    playersScore[playerIndex] += playersPosition[playerIndex]
+
+    return { playersPosition, playersScore }
 }
 
-module.exports = { playGame }
+const playGame = (playersStartPosition, playersStartScore, turn = 1, diceValue = 1, endGame = endGameByScore(1000)) => {
+    if (endGame(playersStartScore)) {
+        return { diceCount: diceValue - 1, playersScore: playersStartScore }
+    }
+    const { playersPosition, playersScore } = updatePlayerPositionScore(playersStartPosition, playersStartScore, turn, (diceValue * 3) + 3)
+    return playGame(playersPosition, playersScore, turn + 1, diceValue + 3, endGame)
+}
+
+const cache = {}
+
+const possibleDiceValues = [
+    [1, 1, 1], [1, 1, 2], [1, 1, 3],
+    [1, 2, 1], [1, 2, 2], [1, 2, 3],
+    [1, 3, 1], [1, 3, 2], [1, 3, 3],
+    [2, 1, 1], [2, 1, 2], [2, 1, 3],
+    [2, 2, 1], [2, 2, 2], [2, 2, 3],
+    [2, 3, 1], [2, 3, 2], [2, 3, 3],
+    [3, 1, 1], [3, 1, 2], [3, 1, 3],
+    [3, 2, 1], [3, 2, 2], [3, 2, 3],
+    [3, 3, 1], [3, 3, 2], [3, 3, 3]
+].map(diceValues => diceValues[0] + diceValues[1] + diceValues[2])
+
+const playMultiVerseGame = (playersStartPosition, playersStartScore, turn = 1, endGame = endGameByScore(21)) => {
+    if (endGame(playersStartScore)) {
+        const maxScore = Math.max(...playersStartScore)
+        return playersStartScore.map(score => (score === maxScore) ? 1 : 0)
+    }
+
+    let playersWins = playersStartScore.map(wins => 0)
+    if (cache[`${playersStartPosition.toString()}-${playersStartScore.toString()}-${turn}`]) {
+        playersWins = cache[`${playersStartPosition.toString()}-${playersStartScore.toString()}-${turn}`]
+    } else {
+        possibleDiceValues.forEach(diceValue => {
+            const { playersPosition, playersScore } = updatePlayerPositionScore(playersStartPosition, playersStartScore, turn, diceValue)
+            const result = playMultiVerseGame(playersPosition, playersScore, turn + 1, endGame)
+            result.forEach((playerWins, playerIndex) => {
+                playersWins[playerIndex] += result[playerIndex]
+            })
+        })
+        cache[`${playersStartPosition.toString()}-${playersStartScore.toString()}-${turn}`] = playersWins
+    }
+
+    return playersWins
+}
+
+module.exports = { playGame, playMultiVerseGame }
--- day21/index.test.js
+++ day21/index.test.js
@@ -1,24 +1,42 @@
 const fs = require('fs')
-const { playGame } = require('./index')
+const { playGame, playMultiVerseGame } = require('./index')
 
 test('basic - play deterministic 100-sided dice', () => {
-    const input = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
+    const playersStartPosition = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
     const expected = 739785
 
-    const { playersScore, diceCount } = playGame(input)
-    const losingPlayerScore = playersScore.sort((a, b) => a - b)[0]
+    const { playersScore, diceCount } = playGame(playersStartPosition, playersStartPosition.map(player => 0))
+    const losingPlayerScore = Math.min(...playersScore)
     const result = losingPlayerScore * diceCount
 
     expect(result).toBe(expected)
 })
 
 test('complete solution - play deterministic 100-sided dice', () => {
-    const input = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
+    const playersStartPosition = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
     const expected = 925605
 
-    const { playersScore, diceCount } = playGame(input)
-    const losingPlayerScore = playersScore.sort((a, b) => a - b)[0]
+    const { playersScore, diceCount } = playGame(playersStartPosition, playersStartPosition.map(player => 0))
+    const losingPlayerScore = Math.min(...playersScore)
     const result = losingPlayerScore * diceCount
 
     expect(result).toBe(expected)
+})
+
+test('basic - play multiverse 3-sided dice', () => {
+    const playersStartPosition = fs.readFileSync('input-basic.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
+    const expected = 444356092776315
+
+    const playersUniverseWins = playMultiVerseGame(playersStartPosition, playersStartPosition.map(player => 0))
+    const winnerPlayerUniverses = Math.max(...playersUniverseWins)
+    expect(winnerPlayerUniverses).toBe(expected)
+})
+
+test('complete solution - play multiverse 3-sided dice', () => {
+    const playersStartPosition = fs.readFileSync('input.txt', 'UTF-8').split('\n').map(player => Number(player.split(':')[1].trim()))
+    const expected = 486638407378784
+
+    const playersUniverseWins = playMultiVerseGame(playersStartPosition, playersStartPosition.map(player => 0))
+    const winnerPlayerUniverses = Math.max(...playersUniverseWins)
+    expect(winnerPlayerUniverses).toBe(expected)
 })
